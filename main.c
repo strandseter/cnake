1e92dac1ad03065f7e288c21c4564811c5d90bb5
andersbss <43789401+andersbss@users.noreply.github.com>
2024-11-09 15:23:42 +0100

    fix flicker length bug

--- main.c
+++ main.c
@@ -5,16 +5,21 @@ int main(void) {
 
   Config cfg = config();
 
-  char current_direction = cfg.directions.right;
-  bool is_game_running = true;
-
+  // The snake
   Vector2 head = { (float)cfg.screenWidth/2, (float)cfg.screenHeight/2 };
   Vector2 *body = malloc(sizeof(Vector2) * 0); 
 
+  // Snake controls
+  int len = 0;
+  char direction = cfg.directions.right;
+
+  // Curren food position
   Vector2 food = { -1, -1 };
 
-  int len = 0;
+  // Game data
   int tick = 0;
+  bool game_running = true;
+  bool did_eat = false;
 
   while (!WindowShouldClose()) {
     BeginDrawing();
@@ -23,11 +28,36 @@ int main(void) {
     //debug_print(head, current_direction);
 
     if (game_over(head, cfg)) {
-      is_game_running = false;
+      game_running = false;
     }
 
-    if (is_game_running) {
+    if (game_running) {
+      // Spawn initial food
+      if (food.x == -1 && food.y == -1) {
+        food.x = (rand() % (cfg.screenWidth / cfg.size)) * cfg.size;
+        food.y = (rand() % (cfg.screenHeight / cfg.size)) * cfg.size;
+      }
+
+      // Spawn food if eaten
+      if (head.x == food.x && head.y == food.y) {
+        food.x = (rand() % (cfg.screenWidth / cfg.size)) * cfg.size;
+        food.y = (rand() % (cfg.screenHeight / cfg.size)) * cfg.size;
+
+        did_eat = true;
+      }
+
+      // Changing direction if user presses arrow
+      change_direction(&direction, cfg.directions);
+
+      // Moving the snake based on tick and speed
       if (tick % cfg.speed == 0) {
+        // If food was eaten, increase the length
+        if (did_eat) {
+          len++;
+          body = realloc(body, sizeof(Vector2) * len);
+          did_eat = false;
+        }
+
         // Shifting body one index to the right to make space for next
         for (int i = len - 1; i > 0; i--) {
           body[i] = body[i - 1];
@@ -39,26 +69,10 @@ int main(void) {
         prev.y = head.y;
         body[0] = prev;
 
-        move(current_direction, &head, cfg);
+        move(direction, &head, cfg);
       }
 
-      change_direction(&current_direction, cfg.directions);
-
-      // Spawn food
-      if (food.x == -1 && food.y == -1) {
-        food.x = (rand() % (cfg.screenWidth / cfg.size)) * cfg.size;
-        food.y = (rand() % (cfg.screenHeight / cfg.size)) * cfg.size;
-      }
-
-      if (head.x == food.x && head.y == food.y) {
-        food.x = (rand() % (cfg.screenWidth / cfg.size)) * cfg.size;
-        food.y = (rand() % (cfg.screenHeight / cfg.size)) * cfg.size;
-
-        len ++;
-        body = realloc(body, sizeof(Vector2) * len + 1);
-      }
-
-      // Draw food
+      // Draw
       DrawRectangle(food.x, food.y, cfg.size, cfg.size, BLUE);
       draw_snake_head(head, cfg);
       draw_snake_body(body, len, cfg);
